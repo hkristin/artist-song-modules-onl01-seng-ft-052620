196bf46580eaedd0d00eecdc576fe59a897d5c6c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-14 20:33:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Song
 extend Memorable::ClassMethods
 extend Findable::ClassMethods
-extend Paramable::ClassMethods
+include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
